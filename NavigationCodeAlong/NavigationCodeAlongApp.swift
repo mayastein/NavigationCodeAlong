f2b007cf4a3c2cbb036095182d09e3e17c046084
Maya Stein <mayarosestein@gmail.com>
2025-04-20 11:19:54 -0400

    Initial Commit

---
+++ NavigationCodeAlong/NavigationCodeAlongApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationCodeAlongApp.swift
+//  NavigationCodeAlong
+//
+//  Created by Maya Stein on 4/20/25.
+//
+
+import SwiftUI
+
+@main
+struct NavigationCodeAlongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NavigationCodeAlongTests/NavigationCodeAlongTests.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationCodeAlongTests.swift
+//  NavigationCodeAlongTests
+//
+//  Created by Maya Stein on 4/20/25.
+//
+
+import Testing
+@testable import NavigationCodeAlong
+
+struct NavigationCodeAlongTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
